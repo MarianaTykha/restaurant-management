291f03370d765a352c30cb2ca289002c44ea1d2b
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2025-03-18 13:33:45 +0200

    Remove unnecessary files and update project structure

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "kitchen",
+    "restaurant_management.kitchen",
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
